92852a080c132011550ef3dea645bb5c7b4e0342
Patrick M <justg.tv@gmail.com>
2011-05-10 19:30:51 -0400

    heroku

---
+++ db/migrate/20110509002407_initial_migration.rb
@@ -0,0 +1,28 @@
+
+#the initial migration -- made for ease in bringing up and tearing down the initial db
+#created May 2011 - JustGtv
+class InitialMigration < ActiveRecord::Migration
+  def self.up
+    
+    create_table :users do |t|
+  		t.string :name   		
+  	end
+    
+    
+    
+  User.create :name => "sam i am"
+  User.create :name => "puddin pie"
+  
+  	
+  end 
+  
+  
+  
+  def self.down
+  	
+  	drop_table :users
+  	
+  	
+  	
+  end
+end
