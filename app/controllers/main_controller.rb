5bb3881fcab435e916cd760c203439a5c948b27c
pat@so <madstacksofcash@gmail.com>
2011-05-10 14:42:38 -0400

    more testing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,6 @@
 #descriptive commenting isnt it?
 class MainController < ApplicationController
   def index    
+    @user = User.all.first
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+	attr_accessor :new_password
+  
+  
+end
